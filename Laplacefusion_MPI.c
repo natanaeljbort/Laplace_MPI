7912301af9c4e9c916446765ca1599960d3bfdf4
natanaeljbort <45204945+natanaeljbort@users.noreply.github.com>
2019-12-13 15:45:47 +0100

    chang1

--- Laplacefusion_MPI.c
+++ Laplacefusion_MPI.c
@@ -6,7 +6,7 @@
 
 float stencil ( float v1, float v2, float v3, float v4)
 {
-  return (v1 + v2 + v3 + v4) * 0.25f;
+  return (v1 + v2 + v3 + v4) / 4;
 }
 
 float max_error ( float prev_error, float old, float new )
